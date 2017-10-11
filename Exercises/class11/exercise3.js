4c059b5a1c83c1af403caa04056b1e0fd0b693c5
jemtca <30645648+jemtca@users.noreply.github.com>
2017-10-11 15:56:38 -0700

    Update exercise3.js

--- Exercises/class11/exercise3.js
+++ Exercises/class11/exercise3.js
@@ -3,12 +3,7 @@ function capitalize(string){
   var newString = string.substring(0,1).toUpperCase();
   
   for(i = 1; i < string.length; i++){
-    if(string[i-1] == " "){
-      newString = newString + string[i].toUpperCase();
-    }
-    else{
       newString = newString + string[i];
-    }
   }
 
   return newString;

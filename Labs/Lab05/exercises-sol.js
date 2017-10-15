7d41deda4ea5c53ecabdd4b206e369c75e091f67
jemtca <30645648+jemtca@users.noreply.github.com>
2017-10-14 20:12:36 -0700

    Update exercises-sol.js

--- Labs/Lab05/exercises-sol.js
+++ Labs/Lab05/exercises-sol.js
@@ -1,1 +1,31 @@
+//Exercise01
+function rotateLeft3(array){
+  
+  var finalArray = new Array(3);
+  
+  finalArray[0] = array[1]; 
+  finalArray[1] = array[2];
+  finalArray[2] = array[0];
+  
+  return array.length == 3 ? finalArray : 
+  "Array lenght is not 3."
+  
+}
 
+//Exercise02
+
+//Exercise03
+
+//Exercise04
+
+//Exercise05
+
+//Exercise06
+
+//Exercise07
+
+//Exercise08
+
+//Exercise09
+
+//Exercise10

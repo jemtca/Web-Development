03c485d7551f39efb6e4417a47b219a40d74c45a
jemtca <30645648+jemtca@users.noreply.github.com>
2017-10-18 14:52:30 -0700

    Update exercises-sol.js

--- Labs/Lab06/exercises-sol.js
+++ Labs/Lab06/exercises-sol.js
@@ -1,1 +1,22 @@
+//Q1
 
+//Q2
+function sumArray(array,number){
+  
+ var result = 0;
+  
+  sum = 0;
+  
+  
+  for(var row = 0; row < number; row++){ 
+    for(var col = 0; col < number; col++){
+      sum = sum + array[row][col];
+    }
+  }
+  
+  return array.length > 0 && number > 0 ? sum : 
+  "Wrong imput/s.";
+  
+}
+
+//Q3

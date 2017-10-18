75d2458d3adf80173a895b8eb3185bc9bc7fe413
jemtca <30645648+jemtca@users.noreply.github.com>
2017-10-18 16:37:16 -0700

    Update exercises-sol.js

--- Labs/Lab06/exercises-sol.js
+++ Labs/Lab06/exercises-sol.js
@@ -2,6 +2,35 @@
 result = dogdeer
 
 //Q1 - b
+function concatenatingArrays(array1,array2){
+  
+  finalArray = "";
+  
+  for(var i = 0; i < array1.length; i++){
+    finalArray = finalArray + "\"";
+    finalArray = finalArray + array1[i];
+    finalArray = finalArray + "\", ";
+  }
+  
+  for(var i = 0; i < array2.length; i++){
+    if(i < array2.length - 1){
+      finalArray = finalArray + "\"";
+      finalArray = finalArray + array2[i];
+      finalArray = finalArray + "\", ";
+    }
+    else{
+      finalArray = finalArray + "\"";
+      finalArray = finalArray + array2[i];
+      finalArray = finalArray + "\"" ;
+    }
+  }
+  
+  return array1.length > 0 || array2.length > 0 ? finalArray : 
+  "Empty array/s.";
+  
+}
+
+children = concatenatingArrays(boys,girls);
 
 //Q1 - c
 
